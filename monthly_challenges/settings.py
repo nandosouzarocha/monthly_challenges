4362739e52f4b69c4da73e8dc4acc1e91ceb7441
Nando Rocha <nando.souza.rocha@gmail.com>
2024-07-25 21:04:43 +0100

    Created a base template to use in multiple pages, created a snippet for a navigation bar as a common resource among pages of the challenges app and made a change in settings.py so the folder templates could be found and referenced without errors.

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,6 +58,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             # BASE_DIR / "challenges" / "templates"
+            os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
